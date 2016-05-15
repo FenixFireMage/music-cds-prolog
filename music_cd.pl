dc8f840d0860c977fd40ccf67e9f6ee3b5324478
Martin Chuckeles <martin@jakubik.info>
2016-05-15 10:37:06 +0200

    Initial commit

---
+++ music_cd.pl
@@ -0,0 +1,15 @@
+/**
+ *
+ * MUSIC CD DATABASE
+ * -----------------
+ *
+ * This program is a database of good ol' music CDs.
+ * It allows the user to add and remove CD, search for
+ * a CD and filter and sort CDs.
+ * It can also save the database to a file and later
+ * restore from this file.
+ *
+ * @author chuckeles
+ * @license MIT
+ *
+ */
